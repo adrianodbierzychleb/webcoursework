b6d955f274e026fbb017f33fb2db628eeb0fee22
Adrian <36075747+adrianodbierzychleb@users.noreply.github.com>
2018-11-26 14:40:27 +0000

    Add files via upload
    uploaded profile templates

--- mysite/match/models.py
+++ mysite/match/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/', default='media/default.jpeg')
+    bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
